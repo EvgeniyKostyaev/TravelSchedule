160fc7f340395f7290d74b03f410c37ce87e58a3
Vladimir Putilov <vladimir.putilov@infobric.com>
2026-01-29 00:46:59 +0400

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Evgeniy Kostyaev on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
