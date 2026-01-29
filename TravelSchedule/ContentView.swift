650ee29c5aa3bdd45845cfce1e85a40149789c9d
Vladimir Putilov <vladimir.putilov@infobric.com>
2026-01-30 01:06:21 +0400

    [sprint 18] Added "openapi.yaml" and "openapi-generator-config.yaml". Created "NearestStationsService"

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +17,38 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            testFetchStations()
+        }
+    }
+    
+    // MARK: - Helper methods
+    private func testFetchStations() {
+        Task {
+            do {
+                let client = Client(
+                    serverURL: try Servers.Server1.url(),
+                    transport: URLSessionTransport()
+                )
+                
+                let service = NearestStationsService(
+                    client: client,
+                    apikey: "11ee95a9-5d01-48d4-a612-52ffa3472cc6"
+                )
+                
+                print("Fetching stations...")
+                let stations = try await service.getNearestStations(
+                    lat: 59.864177,
+                    lng: 30.319163,
+                    distance: 50
+                )
+                
+                print("Successfully fetched stations: \(stations)")
+            } catch {
+                
+                print("Error fetching stations: \(error)")
+            }
+        }
     }
 }
 
